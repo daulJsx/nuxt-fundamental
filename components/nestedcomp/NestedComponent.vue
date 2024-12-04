56e90f79cf02003f89c03bc0ed6081dde93e38cb
daulJsx <mrdaul09@gmail.com>
2024-12-04 13:38:17 +0700

    feat: basic routing and components

---
+++ components/nestedcomp/NestedComponent.vue
@@ -0,0 +1,7 @@
+<template>
+  <CustomTitle>And here's nested component</CustomTitle>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped></style>
---
+++ pages/about.vue
@@ -0,0 +1,8 @@
+<template>
+  <LazyCustomTitle>You are on the About Page</LazyCustomTitle>
+  <LazyNestedcompNestedComponent />
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped></style>
